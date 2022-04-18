a33761d93120e0a7d985d8c74a961cc4465517ea
nikit34 <permikov134@mail.ru>
2022-04-18 06:05:28 +0300

    Add creating new CPU

--- complete_course/sample/random.go
+++ complete_course/sample/random.go
@@ -18,6 +18,43 @@ func randomKeyboardLayout() pb.Keyboard_Layout {
 	}
 }
 
+func randomCPUBrand() string {
+	return randomStringFromSet("Intel", "AMD")
+}
+
+func randomCPUName(brand string) string {
+	if brand == "Intel" {
+		return randomStringFromSet(
+			"Xeon E-2286M",
+			"Core i9-9980HK",
+			"Core i7-97500H",
+			"Core i5-9400F",
+			"Core i3-1005G1",
+		)
+	}
+	return randomStringFromSet(
+		"Ryzen 7 PRO 2700U",
+		"Ryzen 5 PRO 3500U",
+		"Ryzen 3 PRO 3200GE",
+	)
+}
+
+func randomStringFromSet(a ...string) string {
+	n := len(a)
+	if n == 0 {
+		return ""
+	}
+	return a[rand.Intn(n)]
+}
+
+func randomInt(min, max int) int {
+	return min + rand.Intn(max - min + 1)
+}
+
+func randomFloat(min, max float64) float64 {
+	return min + rand.Float64() * (max - min)
+}
+
 func randomBool() bool {
 	return rand.Intn(2) == 1
 }
