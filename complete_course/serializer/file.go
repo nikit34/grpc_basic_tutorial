b8d9d029b72923e6fe6ee9e0654008abcc8228cb
nikit34 <permikov134@mail.ru>
2022-04-19 01:47:28 +0300

    Add convert to JSON

--- complete_course/serializer/file.go
+++ complete_course/serializer/file.go
@@ -8,6 +8,20 @@ import (
 )
 
 
+func WriteProtobufToJSONFile(message proto.Message, filename string) error {
+	data, err := ProtobufToJSON(message)
+	if err != nil {
+		return fmt.Errorf("cannot marshal proto message to JSON: %w", err)
+	}
+
+	err = ioutil.WriteFile(filename, []byte(data), 0644)
+	if err != nil {
+		return fmt.Errorf("cannot write JSON data to file: %w", err)
+	}
+
+	return nil
+}
+
 func WriteProtobufToBinaryFile(message proto.Message, filename string) error {
 	data, err := proto.Marshal(message)
 	if err != nil {
