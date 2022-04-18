920382685045dafa792f51ece019234030da1645
Nikita Permyakov <n.permyakov@aprotech.ru>
2022-04-18 21:20:52 +0300

    Add test

--- complete_course/serializer/file_test.go
+++ complete_course/serializer/file_test.go
@@ -4,13 +4,15 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 
+	"github.com/nikit34/grpc_basic_tutorial/complete_course/pb"
 	"github.com/nikit34/grpc_basic_tutorial/complete_course/sample"
 	"github.com/nikit34/grpc_basic_tutorial/complete_course/serializer"
 )
 
 
-func TestFileSerializer(t *testing.T) {
+func TestToFileSerializer(t *testing.T) {
 	t.Parallel()
 
 	binaryFile := "../tmp/laptop.bin"
@@ -18,5 +20,15 @@ func TestFileSerializer(t *testing.T) {
 	laptop1 := sample.NewLaptop()
 	err := serializer.WriteProtobufToBinaryFile(laptop1, binaryFile)
 	require.NoError(t, err)
+}
 
+func TestFromFileSerializer(t *testing.T){
+	t.Parallel()
+
+	binaryFile := "../tmp/laptop.bin"
+
+	laptop2 := &pb.Laptop{}
+	err := serializer.ReadProtobufFromBinaryFile(binaryFile, laptop2)
+	require.NoError(t, err)
+	require.True(t, proto.Equal(laptop1, laptop2))
 }
