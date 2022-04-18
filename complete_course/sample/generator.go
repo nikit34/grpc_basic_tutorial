dc2575ad966dbe3c2d0ddfedced0108cbf5dda1b
nikit34 <permikov134@mail.ru>
2022-04-18 06:05:28 +0300

    Add creating new CPU

--- complete_course/sample/generator.go
+++ complete_course/sample/generator.go
@@ -10,3 +10,22 @@ func Keyboard() *pb.Keyboard {
 	}
 	return keyboard
 }
+
+func NewCPU() *pb.CPU {
+	brand := randomCPUBrand()
+	name := randomCPUName(brand)
+	numberCores := randomInt(2, 8)
+	numberThreads := randomInt(numberCores, 12)
+	minGhz := randomFloat(2.0, 3.5)
+	maxGhz := randomFloat(minGhz, 5.0)
+
+	cpu := &pb.CPU{
+		Brand: brand,
+		Name: name,
+		NumberCores: uint32(numberCores),
+		NumberThreads: uint32(numberThreads),
+		MinGhz: minGhz,
+		MaxGhz: maxGhz,
+	}
+	return cpu
+}
