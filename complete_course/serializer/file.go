004d1b0d2ed72e7af241da9125ac6ff43bc76aa9
Nikita Permyakov <n.permyakov@aprotech.ru>
2022-04-18 09:57:14 +0300

    Add serializing proto msg to file

---
+++ complete_course/serializer/file.go
@@ -0,0 +1,23 @@
+package serializer
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"google.golang.org/protobuf/proto"
+)
+
+
+func WriteProtobufToBinaryFile(message proto.Message, filename string) error {
+	data, err := proto.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("cannot marshal proto message to binary: %w", err)
+	}
+
+	err = ioutil.WriteFile(filename, data, 0644)
+	if err != nil {
+		return fmt.Errorf("cannot write binary data to file: %w", err)
+	}
+
+	return nil
+}
