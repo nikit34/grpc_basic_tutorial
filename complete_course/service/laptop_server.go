cd0dc8f1b892a83dbb3ea771aa0a43cf69a4310f
nikit34 <permikov134@mail.ru>
2022-04-19 23:30:37 +0300

    Bugfix for passing tests

--- complete_course/service/laptop_server.go
+++ complete_course/service/laptop_server.go
@@ -17,8 +17,8 @@ type LaptopServer struct {
 	Store LaptopStore
 }
 
-func NewLaptopServer() *LaptopServer {
-	return &LaptopServer{}
+func NewLaptopServer(store LaptopStore) *LaptopServer {
+	return &LaptopServer{store}
 }
 
 func (server *LaptopServer) CreateLaptop(
