c22ffe7217c4b00cac1e4210ba2b0137011fb005
Nikita Permyakov <n.permyakov@aprotech.ru>
2022-04-19 18:52:56 +0300

    Add test server

---
+++ complete_course/service/laptop_server_test.go
@@ -0,0 +1,94 @@
+package service_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/nikit34/grpc_basic_tutorial/complete_course/pb"
+	"github.com/nikit34/grpc_basic_tutorial/complete_course/sample"
+	"github.com/nikit34/grpc_basic_tutorial/complete_course/service"
+)
+
+
+func TestServerCreateLaptop(t *testing.T) {
+	t.Parallel()
+
+	laptopNoID := sample.NewLaptop()
+	laptopNoID.Id = ""
+
+	laptopInvalidID := sample.NewLaptop()
+	laptopInvalidID.Id = "invalid-uuid"
+
+	laptopDuplicateID := sample.NewLaptop()
+	storeDuplicateID := service.NewInMemoryLaptopStore()
+	err := storeDuplicateID.Save(laptopDuplicateID)
+	require.Nil(t, err)
+
+	testCases := []struct {
+		name string
+		laptop *pb.Laptop
+		store service.LaptopStore
+		code codes.Code
+	} {
+		{
+			name: "success_with_id",
+			laptop: sample.NewLaptop(),
+			store: service.NewInMemoryLaptopStore(),
+			code: codes.OK,
+		},
+		{
+			name: "success_no_id",
+			laptop: laptopNoID,
+			store: service.NewInMemoryLaptopStore(),
+			code: codes.OK,
+		},
+		{
+			name: "success_invalid_id",
+			laptop: laptopInvalidID,
+			store: service.NewInMemoryLaptopStore(),
+			code: codes.InvalidArgument,
+		},
+		{
+			name: "success_duplicate_id",
+			laptop: laptopDuplicateID,
+			store: service.NewInMemoryLaptopStore(),
+			code: codes.AlreadyExists,
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func (t *testing.T) {
+			t.Parallel()
+
+			req := &pb.CreateLaptopRequest{
+				Laptop: tc.laptop,
+			}
+
+			server := service.NewLaptopServer(tc.store)
+			res, err := server.CreateLaptop(context.Background(), req)
+
+			if tc.code == codes.OK {
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				require.NotEmpty(t, res.Id)
+
+				if len(tc.laptop.Id) > 0 {
+					require.Equal(t, tc.laptop.Id, res.Id)
+				}
+			} else {
+				require.Error(t, err)
+				require.Nil(t, res)
+
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, tc.code, st.Code())
+			}
+		})
+	}
+}
