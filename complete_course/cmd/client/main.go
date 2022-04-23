86d05923a78b543f0076007f99755cb325a4bb63
nikit34 <permikov134@mail.ru>
2022-04-23 11:30:40 +0300

    Add dial clients

--- complete_course/cmd/client/main.go
+++ complete_course/cmd/client/main.go
@@ -1,1 +1,46 @@
 package client
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/nikit34/grpc_basic_tutorial/complete_course/pb"
+	"github.com/nikit34/grpc_basic_tutorial/complete_course/sample"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+
+func main() {
+	serverAddress := flag.String("address", "", "server address")
+	flag.Parse()
+
+	log.Printf("dial server %s", *serverAddress)
+
+	conn, err := grpc.Dial(*serverAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("cannot dial server: ", err)
+	}
+
+	laptopClient := pb.NewLaptopServiceClient(conn)
+
+	laptop := sample.NewLaptop()
+	req := &pb.CreateLaptopRequest{
+		Laptop: laptop,
+	}
+
+	res, err :=laptopClient.CreateLaptop(context.Background(), req)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok && st.Code() == codes.AlreadyExists {
+			log.Fatal("laptop already exists")
+		} else {
+			log.Fatal("cannot create laptop: ", err)
+		}
+		return
+	}
+
+	log.Printf("create laptop with id: %s", res.Id)
+}
