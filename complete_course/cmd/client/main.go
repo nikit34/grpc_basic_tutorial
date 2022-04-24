714ffaa8bfbcc3de1c5bc5fc3a24cc9352886a15
nikit34 <permikov134@mail.ru>
2022-04-24 09:44:38 +0300

    Add errors handlers

--- complete_course/cmd/client/main.go
+++ complete_course/cmd/client/main.go
@@ -35,7 +35,11 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
 	defer cancel()
 
+<<<<<<< HEAD
 	res, err := laptopClient.CreateLaptop(ctx, req)
+=======
+	res, err :=laptopClient.CreateLaptop(ctx, req)
+>>>>>>> 184e43b... Add errors handlers
 	if err != nil {
 		st, ok := status.FromError(err)
 		if ok && st.Code() == codes.AlreadyExists {
