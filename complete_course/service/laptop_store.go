2a1a58ee3b92d0a40e36460191a5f3f185b59090
Никита Пермяков <permikov134@mail.ru>
2022-04-26 08:18:28 +0000

    Add deep copy function

--- complete_course/service/laptop_store.go
+++ complete_course/service/laptop_store.go
@@ -38,10 +38,9 @@ func (store *InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
 		return ErrAlreadyExists
 	}
 
-	other := &pb.Laptop{}
-	err := copier.Copy(other, laptop)
+	other, err := deepCopy(laptop)
 	if err != nil {
-		return fmt.Errorf("cannot copy laptop data: %w", err)
+		return err
 	}
 
 	store.data[other.Id] = other
@@ -57,13 +56,7 @@ func (store *InMemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
 		return nil, nil
 	}
 
-	other := &pb.Laptop{}
-	err := copier.Copy(other, laptop)
-	if err != nil {
-		return nil, fmt.Errorf("cannot copy laptop data: %w", err)
-	}
-
-	return other, nil
+	return deepCopy(laptop)
 }
 
 func (store *InMemoryLaptopStore) Search(
@@ -72,4 +65,65 @@ func (store *InMemoryLaptopStore) Search(
 ) error {
 	store.mutex.RLock()
 	defer store.mutex.RUnlock()
+
+	for _, laptop := range store.data {
+		if isQualified(filter, laptop) {
+			other, err := deepCopy(laptop)
+			if err != nil {
+				return err
+			}
+
+			err = found(other)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func isQualified(filter *pb.Filter, laptop *pb.Laptop) bool {
+	if laptop.GetPriceUsd() > filter.GetMaxPriceUsd() {
+		return false
+	}
+
+	if laptop.GetCpu().GetNumberCores() < filter.GetMinCpuCores() {
+		return false
+	}
+
+	if laptop.GetCpu().GetMinGhz() < filter.GetMinCpuGhz() {
+
+	}
+
+	if toBit(laptop.GetRam()) < toBit(filter.GetMinRam()) {
+		return false
+	}
+
+	return true
+}
+
+func toBit(memory *pb.Memory) uint64 {
+	value := memory.GetValue()
+
+	switch memory.GetUnit() {
+	case pb.Memory_BIT:
+		return value
+	case pb.Memory_BYTE:
+		return value << 3
+	case pb.Memory_KILOBYTE:
+		return value << 13
+	case pb.Memory_TERABYTE:
+		return value << 43
+	default:
+		return 0
+	}
+}
+
+func deepCopy(laptop *pb.Laptop) (*pb.Laptop, error) {
+	other := &pb.Laptop{}
+	err := copier.Copy(other, laptop)
+	if err != nil {
+		return nil, fmt.Errorf("cannot copy laptop data: %w", err)
+	}
+	return other, nil
 }
