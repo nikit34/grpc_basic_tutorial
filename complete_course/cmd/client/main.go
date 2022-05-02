dcaf7045e0a9ed8de88dc0fe5fb57d252d7e9b2f
Nikita Permyakov <n.permyakov@aprotech.ru>
2022-05-02 20:32:29 +0300

    Add upload image method to client

--- complete_course/cmd/client/main.go
+++ complete_course/cmd/client/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
-	"log"
-	"time"
 	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/nikit34/grpc_basic_tutorial/complete_course/pb"
 	"github.com/nikit34/grpc_basic_tutorial/complete_course/sample"
@@ -71,7 +74,63 @@ func searchLaptop(laptopClient pb.LaptopServiceClient, filter *pb.Filter) {
 }
 
 func uploadImage(laptopClient pb.LaptopServiceClient, laptopID string, imagePath string) {
-	
+	file, err := os.Open(imagePath)
+	if err != nil {
+		log.Fatalf("cannot open image file: %v", err)
+	}
+	defer file.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	stream, err := laptopClient.UploadImage(ctx)
+	if err != nil {
+		log.Fatal("cannot upload image: ", err)
+	}
+
+	req := &pb.UploadImageRequest{
+		Data: &pb.UploadImageRequest_Info{
+			Info: &pb.ImageInfo{
+				LaptopId: laptopID,
+				ImageType: filepath.Ext(imagePath),
+			},
+		},
+	}
+	err = stream.Send(req)
+	if err != nil {
+		log.Fatal("cannot send image info: ", err)
+	}
+
+	reader := bufio.NewReader(file)
+	buffer := make([]byte, 1024)
+
+	for {
+		n, err := reader.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal("cannot read chunk to buffer: ", err)
+		}
+
+		req := &pb.UploadImageRequest{
+			Data: &pb.UploadImageRequest_ChunkData{
+				ChunkData: buffer[:n],
+			},
+		}
+
+		err = stream.Send(req)
+		if err != nil {
+			log.Fatal("cannot send chunk to server: ", err)
+		}
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatal("cannot receive response: ", err)
+	}
+
+	log.Printf("image uploaded with id: %s, size: %d", res.GetId(), res.GetSize())
 }
 
 func testCreateLaptop(laptopClient pb.LaptopServiceClient) {
@@ -114,6 +173,5 @@ func main() {
 	}
 
 	laptopClient := pb.NewLaptopServiceClient(conn)
-
-
+	testUploadImage(laptopClient)
 }
