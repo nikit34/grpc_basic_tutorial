a36fd390accf5a78981eeb97bde43a227f798f55
Nikita Permyakov <n.permyakov@aprotech.ru>
2022-05-03 19:02:40 +0300

    Add body for methods Save and Find on user store file

--- complete_course/service/user_store.go
+++ complete_course/service/user_store.go
@@ -20,9 +20,24 @@ func NewInMemoryUserStore() *InMemoryUserStore {
 }
 
 func (store *InMemoryUserStore) Save(user *User) error {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
 
+	if store.users[user.Username] != nil {
+		return ErrAlreadyExists
+	}
+
+	store.users[user.Username] = user.Clone()
+	return nil
 }
 
 func (store *InMemoryUserStore) Find(username string) (*User, error) {
-	
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	user := store.users[username]
+	if user == nil {
+		return nil, nil
+	}
+	return user.Clone(), nil
 }
